422f946dbb1cb1cabbd5ef4b94b9f51a70acab73
lvyuanyin <460438850@qq.com>
2018-07-27 10:03:09 +0800

    create travis-ci

--- src/controller/api.js
+++ src/controller/api.js
@@ -2,6 +2,10 @@ const BaseRest = require('./rest.js');
 
 module.exports = class extends BaseRest {
     async testAction(){
-        return this.success('test success')
+        // return this.success('test success')
+        return {
+            user: 'name',
+            age: 11
+        }
     }
 };
