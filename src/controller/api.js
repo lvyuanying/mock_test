7e6895c73ad4bcf350943831e99525054bfbb2a1
lvyuanyin <460438850@qq.com>
2018-07-27 10:14:31 +0800

    update

--- src/controller/api.js
+++ src/controller/api.js
@@ -3,7 +3,7 @@ const BaseRest = require('./rest.js');
 module.exports = class extends BaseRest {
     async testAction(){
         // return this.success('test success')
-        return this.body = {
+        this.body = {
             user: 'name',
             age: 11
         }
